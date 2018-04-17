b31867b31192282a11c66e191e573dbb74065e84
Tiffany Schureman <tschureman@gmail.com>
2018-04-17 16:35:02 -0500

    styling header

---
+++ sim-music/src/Header/Header.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+import Songster from '../../src/Assets/Songster.png';
+import "../Styles/Header.css";
+
+class Header extends Component{
+    render(){
+        return(
+            <header>
+                <div><img src={Songster} className='logo' alt='Songster Logo'/></div>
+                <h2> all playlists </h2> <h2> this playlist: 5 songs </h2>
+            </header>
+        )
+    }
+}
+
+export default Header;
+
