b0716dfd693c3ec3064ed5875174dab4897224cd
Tiffany Schureman <tschureman@gmail.com>
2018-04-17 13:53:48 -0500

    set up server and started sessions

---
+++ sim-music/server/index.js
@@ -0,0 +1,49 @@
+require ('dotenv').config();
+const express = require('express');
+const {json} = require ('body-parser');
+const session = require('express-session');
+const cors = require ('cors');
+const passport = require('passport');
+const massive = require ('massive');
+const {
+    CONNECTION_STRING,
+    DOMAIN,
+    CLIENT_ID,
+    CLIENT_SECRET,
+    SESSION_SECRET
+  } = process.env;
+
+const port = 3001;
+
+const app = express();
+
+massive(CONNECTION_STRING).then(db =>{
+    app.set('db', db);
+})
+    .catch(console.log);
+
+app.use(json());
+app.use(cors());
+
+app.use(
+    session({
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser((user,done)=>{
+    done(null,user);
+})
+
+passport.deserializeUser((user,done )=>{
+    done(null,user);
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+})
