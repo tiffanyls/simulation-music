aa29c4d0a0f3945ce6c2c99616997923128406bc
Tiffany Schureman <tschureman@gmail.com>
2018-04-18 12:16:51 -0500

    styling add song

--- sim-music/src/Add Song View/AddSong.js
+++ sim-music/src/Add Song View/AddSong.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import {addSong} from '../../src/ducks/reducer';
+import Header from '../Header/Header';
+import '../Styles/AddSong.css';
 
 class AddSong extends Component {
     constructor(){
@@ -9,13 +11,17 @@ class AddSong extends Component {
 }
 
 render(){
-    return(
+    return( <div>
+        <Header />
+        <div>
         <section>
             <h2 id="newsong"> New song </h2>
             <input type="text" placeholder="song"></input>
             <input type="text" placeholder="artist"></input>
             <input type="text" placeholder="album"></input>
         </section>
+        </div>
+        </div>
     )
 }
 }
